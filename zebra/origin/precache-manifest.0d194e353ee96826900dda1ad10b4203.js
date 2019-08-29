1af7c4be96cfd9ef26320292a88a51a43149617a
Bart Bogaerts <bart.bogaerts@cs.kuleuven.be>
2019-08-29 15:44:15 +0200

    origin

---
+++ zebra/origin/precache-manifest.0d194e353ee96826900dda1ad10b4203.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcfd01ee2ab35632e7ec1f719ad458d6",
+    "url": "/zebra/origin/index.html"
+  },
+  {
+    "revision": "93cc418a786b6bac13ac",
+    "url": "/zebra/origin/static/css/main.59910678.chunk.css"
+  },
+  {
+    "revision": "31989ecb0243373f36bb",
+    "url": "/zebra/origin/static/js/2.71a66cf6.chunk.js"
+  },
+  {
+    "revision": "93cc418a786b6bac13ac",
+    "url": "/zebra/origin/static/js/main.97b648d3.chunk.js"
+  },
+  {
+    "revision": "891177c99c8f44844e35",
+    "url": "/zebra/origin/static/js/runtime~main.f6f8c1cf.js"
+  }
+]);
