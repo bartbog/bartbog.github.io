1710ebfd333476aa67c11971051869d4d5625b2a
Bart Bogaerts <bart.bogaerts@cs.kuleuven.be>
2019-08-29 16:02:25 +0200

    tutorial

---
+++ zebra/tutorial/precache-manifest.56d428c16fbc62774bbc36fa7572a5b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f84a5efbc21546378711ccd4b9fe0156",
+    "url": "/zebra/tutorial/index.html"
+  },
+  {
+    "revision": "e6fb673778bd08716bf8",
+    "url": "/zebra/tutorial/static/css/main.59910678.chunk.css"
+  },
+  {
+    "revision": "31989ecb0243373f36bb",
+    "url": "/zebra/tutorial/static/js/2.71a66cf6.chunk.js"
+  },
+  {
+    "revision": "e6fb673778bd08716bf8",
+    "url": "/zebra/tutorial/static/js/main.f45e6816.chunk.js"
+  },
+  {
+    "revision": "b18c327d17da96ccfc05",
+    "url": "/zebra/tutorial/static/js/runtime~main.fa5c39d3.js"
+  }
+]);
