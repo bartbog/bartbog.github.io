30dabbe710837a60ee000da32fc013802b3387ea
Bart Bogaerts <bart.bogaerts@cs.kuleuven.be>
2019-10-02 09:32:21 +0200

    update

---
+++ zebra/tourists/precache-manifest.cd9b86a1a098f316222cd4854fa8de80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a3c77dc504000935f7334fb105eeda8",
+    "url": "/zebra/tourists/index.html"
+  },
+  {
+    "revision": "9d183a614daf9753dd0a",
+    "url": "/zebra/tourists/static/css/main.88f502c3.chunk.css"
+  },
+  {
+    "revision": "efb629678e2a2e34fe97",
+    "url": "/zebra/tourists/static/js/2.581f262e.chunk.js"
+  },
+  {
+    "revision": "9d183a614daf9753dd0a",
+    "url": "/zebra/tourists/static/js/main.217314a8.chunk.js"
+  },
+  {
+    "revision": "406d656333f6b80f4562",
+    "url": "/zebra/tourists/static/js/runtime~main.79e6194f.js"
+  }
+]);
